cee1dc5caab454af4e4d09f20153733fcbc3bb83
Ingrid Hernandez <ingrid_Hernandez@unitec.edu>
2022-10-28 14:08:39 -0600

    avance 2

--- src/lab3p2_ingridhernandez_12141186/Lab3P2_IngridHernandez_12141186.java
+++ src/lab3p2_ingridhernandez_12141186/Lab3P2_IngridHernandez_12141186.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  */
 public class Lab3P2_IngridHernandez_12141186 {
 
-    
+    static ArrayList<Personaje> personaje = new ArrayList();
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner lea = new Scanner(System.in);
@@ -31,7 +31,58 @@ public class Lab3P2_IngridHernandez_12141186 {
             int opcion = lea.nextInt();
             switch (opcion){
                 case 1:{
-                    
+                    boolean centinela1 = true;
+
+                    while (centinela1 == true) {
+                        System.out.println("1)Clerigo ");
+                        System.out.println("2)Barbaro ");
+                        System.out.println("3)Mago");
+                        System.out.println("4)Picaro");
+                        System.out.print("Ingreseel personaje que desea: ");
+                        int op = lea.nextInt();
+                        switch (op) {
+                            case 1: {
+                                System.out.println("Ingrese el Nombre del presonaje: ");
+                                String nom = lea.next();
+                                System.out.println("Ingrese la el tipo de raza:");
+                                System.out.println("1. Mediano");
+                                System.out.println("2. Enano");
+                                System.out.println("3. Elfo");
+                                System.out.println("4. Humano");
+                                int tr = lea.nextInt();
+                                String raza;
+                                if(tr == 1){
+                                    raza = "Mediano";
+                                }else if( tr == 2){
+                                    raza = "Enano";
+                                }else if(tr == 3){
+                                    raza = "Elfo";
+                                }else if( tr == 4){
+                                    raza = "Humano";
+                                }else {
+                                    System.out.println("no existe esa raza");
+                                }
+                                System.out.println("Ingrese la estatura del personaje: ");
+                                double estatura = lea.nextInt();
+                                System.out.println("");
+                            }//fin del case 1
+                            break;
+                            case 2: {
+
+                            }//fin del case 2
+                            break;
+                            case 3: {
+
+                            }//fin del case 3
+                            break;
+                            case 4:
+                                centinela1 = false;
+                                break;
+
+                            default:
+                                System.out.println("Opcion Incorrecta");
+                        }//fin del switch
+                    }//fin while
                 }//fin del case 1
                 break;
                 case 2:{
--- src/lab3p2_ingridhernandez_12141186/Personaje.java
+++ src/lab3p2_ingridhernandez_12141186/Personaje.java
@@ -1,3 +1,5 @@
+package lab3p2_ingridhernandez_12141186;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,12 +11,12 @@
  */
 public class Personaje {
     private String nombre;
-    private int raza;
+    private String raza;
     private double estaura;
     private double peso;
     private int edad;
     private String descripcion;
-    private int nacionalidad;
+    private String nacionalidad;
     private int Tp;
     
 }
